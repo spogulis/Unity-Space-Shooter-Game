f0e1463303f7c7f148de8c0c66524b13ec998246
Renārs Grebežs <originalchc@gmail.com>
2018-10-21 23:55:16 +0200

    Game is playable with a single repeated wave
    TODO: 
    1) Score reset after destroying ship
    2) Replay button
    3) Different waves
    4) Enemies

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text scoreText;
+    private int score;
+
+    private void Start()
+    {
+        score = 0;
+        updateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        while (true)
+        {
+            for (int i = 0; i <hazardCount; i++)
+            {
+                yield return new WaitForSeconds(startWait);
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnLocation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnLocation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+
+    public void AddScore(int newValue)
+    {
+        score += newValue;
+        updateScore();
+    }
+
+    void updateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
---
+++ Assets/Scripts/PlController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+
+public class PlController : MonoBehaviour {
+
+    public Boundary boundary;
+    public float speed;
+    public float tilt;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    
+    
+    private Rigidbody rb;
+    private float nextFire;
+    private AudioSource audio;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            audio = GetComponent<AudioSource>();
+            audio.Play();
+        }    
+    }
+
+
+    private void FixedUpdate() 
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
+        rb.velocity = movement * speed;
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
